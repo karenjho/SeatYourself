08aa6b2c75e648a6222f6bd348786c8a4c6fb585
Patrick Cruikshank <patrick.cruikshank@gmail.com>
2016-02-03 15:18:55 -0500

    add methods to restaurantscontroller for RESTful routes

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,2 +1,49 @@
 class RestaurantsController < ApplicationController
+  before_action :load_restaurant, only: [:show, :edit, :update, :destroy]
+
+  def index
+    @restaurants = Restaurant.all
+  end
+
+  def show
+  end
+
+  def new
+    @restaurant = Restaurant.new
+  end
+
+  def create
+    @restaurant = Restaurant.new(restaurant_params)
+    if @restaurant.save
+      redirect_to restaurant_path(@restaurant)
+    else
+      render :new
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if @restaurant.update_attributes(restaurant_params)
+      redirect_to restaurant_path(@restaurant)
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @restaurant.destroy
+    redirect_to root_path
+  end
+
+  private
+  def restaurant_params
+    params.require(:restaurant).permit(:name, :capacity, :address, :phone, :url, :photo_url, :owner_id)
+  end
+
+  def load_restaurant
+    @restaurant = Restaurant.find(params[:id])
+  end
+
 end
