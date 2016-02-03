61532bf1fa4eebd8b52f0884296aad5754932d92
Patrick Cruikshank <patrick.cruikshank@gmail.com>
2016-02-03 14:00:53 -0500

    creating empty controllers and routes for all classes, plus routes

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ActiveRecord::Base
+  has_secure_password
   has_many :reservations
   has_many :owned_restaurants, class_name: "Restaurant", foreign_key: "owner_id"
   has_many :restaurants, through: :reservations
