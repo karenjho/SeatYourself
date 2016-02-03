3102f4bb67a6f9717463935a2fa88384ce6aedb8
ndoak <natalieannedoak@gmail.com>
2016-02-03 15:21:26 -0500

    Add controller methods to Sessions controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,2 +1,20 @@
 class SessionsController < ApplicationController
+
+def new
+end
+
+def create
+  user = User.find_by(params[:email, params[:email])
+    if user &. user.authenticate(params[:password])
+      session[:user_id] = user.id
+      redirect_to restaurants_url, notice: "Thanks for logged in"
+    else
+      render :new
+    end
+end
+
+def destroy
+  session[:user_id] = nil
+  redirect_to restaurants_url, notice: "Logout Successful"
+end
 end
