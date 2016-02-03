a7e202cff2d0943be411c97a23b34ce1e37f033a
Jonathan Alorda <jonathan.alorda@gmail.com>
2016-02-03 16:43:13 -0500

    Add form for user create in users#new, add views users#show and users#new, user account create redirects to users#show

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,7 +7,7 @@ class UsersController < ApplicationController
   def create
     @user = User.create(user_params)
     if @user.save
-      redirect_to user_path
+      redirect_to user_path(@user)
     else
       render "new"
     end
