7fed0bc601ef3b0f662967c5bfc5f7d74123328a
Jonathan Alorda <jonathan.alorda@gmail.com>
2016-02-03 16:38:32 -0500

    Add users#show view. Need to fix redirect after account sign up

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,7 +7,7 @@ class UsersController < ApplicationController
   def create
     @user = User.create(user_params)
     if @user.save
-      redirect_to users_path
+      redirect_to user_path
     else
       render "new"
     end
@@ -39,7 +39,7 @@ class UsersController < ApplicationController
   private
 
   def user_params
-    params.require(:user).permit(:username, :email, :password_confirmation)
+    params.require(:user).permit(:username, :email, :password, :password_confirmation)
   end
 
 end
