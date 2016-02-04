54c0bb9879a59d4df1b6fddb32396530949b8e60
ndoak <natalieannedoak@gmail.com>
2016-02-04 17:17:22 -0500

    New migration

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160202213412) do
+ActiveRecord::Schema.define(version: 20160204205055) do
 
   create_table "reservations", force: :cascade do |t|
     t.datetime "date_time"
@@ -38,6 +38,11 @@ ActiveRecord::Schema.define(version: 20160202213412) do
   end
 
   add_index "restaurants", ["owner_id"], name: "index_restaurants_on_owner_id"
+
+  create_table "reviews", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "email"
