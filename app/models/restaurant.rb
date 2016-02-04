5f73ce96cc48edfa7046b79b6c8d672dce389d66
karenjho <hokarenj@gmail.com>
2016-02-04 14:04:52 -0500

    Include FactoryGirl gem and create restaurant tests

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -2,4 +2,11 @@ class Restaurant < ActiveRecord::Base
   belongs_to :owner, class_name: "User"
   has_many :reservations
   has_many :users, through: :reservations
+
+  def available?(party_size, time)
+    all_reservations = reservations.where("time >= ? and time < ?", time.beginning_of_hour, time.end_of_hour).sum(:party_size)
+    available_capacity = capacity - all_reservations
+
+    party_size > 0 && party_size <= available_capacity
+  end
 end
--- test/models/restaurant_test.rb
+++ test/models/restaurant_test.rb
@@ -1,7 +1,35 @@
 require 'test_helper'
 
 class RestaurantTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  setup do
+    @restaurant = FactoryGirl.create(:restaurant)
+    @time = Time.now
+  end
+
+  test "restaurant is available" do
+    assert @restaurant.available?(10, @time)
+  end
+
+  test "restaurant is not available" do
+    assert_not @restaurant.available?(200, @time)
+  end
+
+  test "restaurant is available for party_size of 0" do
+    assert_not @restaurant.available?(0, @time)
+  end
+
+  test "restaurant is available given a reservation" do
+    FactoryGirl.create(:reservation, party_size: 10, restaurant: @restaurant)
+    assert @restaurant.available?(80, @time)
+  end
+
+  test "restaurant is not available given a reservation" do
+    FactoryGirl.create(:reservation, party_size: 149, restaurant: @restaurant)
+    assert @restaurant.available?(10, @time)
+  end
+
+  test "restaurant is available with reservations at different times" do
+    FactoryGirl.create(:reservation, @restaurant, time: @time, party_size: 99 )
+    assert @restaurant.available?(10, @time)
+  end
 end
