4e8b58031516762e78fb4d3e607fcf82e8693355
Patrick Cruikshank <patrick.cruikshank@gmail.com>
2016-02-06 18:43:33 -0500

    add delete link to restaurant page if you are the owner, adn dependant destroy to user model so owned restaurants are removed if user is deleted

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,6 @@ class User < ActiveRecord::Base
   has_secure_password
   has_many :reviews, dependent: :destroy
   has_many :reservations, dependent: :destroy
-  has_many :owned_restaurants, class_name: "Restaurant", foreign_key: "owner_id"
+  has_many :owned_restaurants, class_name: "Restaurant", foreign_key: "owner_id", dependent: :destroy
   has_many :restaurants, through: :reservations
 end
